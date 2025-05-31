8cad728eb4d16f8156bc2dc2d6b4863be5422c0d
EduardoSilva03 <eduaardo.silva03@gmail.com>
2025-05-31 15:18:35 -0300

    Desafio inicial 08

---
+++ 8.py
@@ -0,0 +1,7 @@
+# Inversor de Strings: Crie um programa que receba uma palavra ou frase e a imprima invertida. (Ex: "python" vira "nohtyp").
+
+frase = input("Digite uma palavra ou frase: ")
+
+frase = frase[::-1]
+
+print(f"Frase invertida: {frase}")
