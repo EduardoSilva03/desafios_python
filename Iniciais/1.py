dbe7460562fd82a00df10edaf3deb2bd5223c512
EduardoSilva03 <eduaardo.silva03@gmail.com>
2025-05-31 14:48:52 -0300

    Desafio inicial 03

--- Iniciais/1.py
+++
@@ -1,7 +0,0 @@
-# 1. Soma de Dois Números: Escreva um programa que solicite dois números ao usuário e imprima a soma deles.
-
-print("Escreva dois números para somar: ")
-num1 = float(input("Número 1: "))
-num2 = float(input("Número 2: "))
-soma = num1 + num2
-print(f"A soma de {num1} e {num2} é {soma}.")
--- Iniciais/2.py
+++
@@ -1,8 +0,0 @@
-# 2. Número Par ou Ímpar: Crie um programa que peça um número e diga se ele é par ou ímpar.
-
-print("Digite um número para verificar se é par ou ímpar: ")
-num = int(input("Número: "))
-if num % 2 == 0:
-    print(f"O número {num} é par.")
-else:
-    print(f"O número {num} é ímpar.")
